2736af72173151a2aaefc8cfd33c416cf47bb181
salman18 <salmanaleemkhan786@gmail.com>
2020-08-25 00:57:42 +0530

    redux-thunk middleware

--- src/index.js
+++ src/index.js
@@ -11,21 +11,26 @@ store.subscribe(() => {
 	console.log('Store changed',store.getState())
 });
 
-store.dispatch(userAdded({id: 1,name: 'Salman'}));
-store.dispatch(userAdded({id: 1,name: 'Taj'}));
+store.dispatch((dispatch, getState) => {
+	dispatch({type: "bugRecieved",bugs: [1,2,3]})
+});
 
 
-store.dispatch(bugAdded({description: 'Bug1'}));
-store.dispatch(bugAdded({description: 'Bug2'}));
-store.dispatch(bugAdded({description: 'Bug3'}));
-store.dispatch(bugResolved({id: 1}));
-
-store.dispatch(bugAssignToUser({bugId: 1,userId: 1}));
+// store.dispatch(userAdded({id: 1,name: 'Salman'}));
+// store.dispatch(userAdded({id: 1,name: 'Taj'}));
 
 
-store.dispatch(projectAdded({name: 'Project 1'}));
+// store.dispatch(bugAdded({description: 'Bug1'}));
+// store.dispatch(bugAdded({description: 'Bug2'}));
+// store.dispatch(bugAdded({description: 'Bug3'}));
+// store.dispatch(bugResolved({id: 1}));
+
+// store.dispatch(bugAssignToUser({bugId: 1,userId: 1}));
 
 
-const bugs = getBugsByUser(1)(store.getState());
-console.log(bugs);
+// store.dispatch(projectAdded({name: 'Project 1'}));
+
+
+// const bugs = getBugsByUser(1)(store.getState());
+// console.log(bugs);
 
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,11 @@
-import {configureStore} from '@reduxjs/toolkit'
-import reducer from './reducer'
+import {configureStore,getDefaultMiddleware} from '@reduxjs/toolkit'
+import reducer from './reducer';
+import logg from './middleware/logger';
+// import func from './middleware/func';
 
 export default function() {
 	return configureStore({
-		reducer
+		reducer,
+		middleware: [...getDefaultMiddleware(),logg]
 	});
 }
