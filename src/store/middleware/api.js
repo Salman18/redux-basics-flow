e9f9c72ee9365b2eda69d431a08e0925dc1799a8
salman18 <salmanaleemkhan786@gmail.com>
2020-10-17 00:28:09 +0530

    apiMiddleware added for calling api

---
+++ src/store/middleware/api.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import * as actions from '../api';
+
+const api = ({dispatch}) => next => async action => {
+	if(action.type !== actions.apiCallBegan.type) return next(action);
+	next(action);
+	const {url,onError,onSuccess,data,method} = action.payload;
+	try {
+		const response = await axios.request({
+			baseURL: 'http://localhost:9001/api',
+			url,
+			method,
+			data
+		});
+		//General
+		dispatch(actions.apiCallSuccess(response.data));
+		//Specific
+		if(onSuccess)
+			dispatch({type: onSuccess,payload: response.data});
+	} catch(error) {
+		dispatch(actions.apiCallFailed(error))
+		// Specific
+		if(onError)
+			dispatch({type: onError,payload: error});
+	}
+
+};
+
+export default api;
